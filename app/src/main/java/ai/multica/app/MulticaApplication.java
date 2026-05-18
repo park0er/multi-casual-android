362cd3078ced937c443faf3d752c00886f020936
Xisheng-Zhao <parker.z@live.cn>
2026-05-18 17:46:23 +0800

    Add analytics tab view analytics

---
+++ app/src/main/java/ai/multica/app/MulticaApplication.java
@@ -0,0 +1,10 @@
+package ai.multica.app;
+
+import android.app.Application;
+
+public final class MulticaApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
